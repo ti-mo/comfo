fa9e284f7da19b223460a63c9da1ebe161edce9b
Timo Beckers <timo@incline.eu>
2017-10-30 20:37:46 +0100

    libcomfo - message: test coverage for EncodeGetRequest and EncodeSetRequest

--- libcomfo/message_test.go
+++ libcomfo/message_test.go
@@ -1,9 +1,7 @@
 package libcomfo
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
 	"reflect"
 	"testing"
 )
@@ -107,6 +105,187 @@ func TestTemperature_UnmarshalBinary(t *testing.T) {
 
 			if want, got := tt.temp, temp; !reflect.DeepEqual(want, got) {
 				t.Fatalf("unexpected temperature unmarshal:\n- want: %v\n-  got: %v",
+					want, got)
+			}
+		})
+	}
+}
+
+// Simple test that calls EncodeGetRequest on a getRequest,
+// which wraps an int in a struct and sets Expect to true.
+func TestEncodeGetRequest(t *testing.T) {
+
+	pkt := EncodeGetRequest(getBootloader)
+
+	if want, got := getBootloader, getRequest(pkt.Command); want != got {
+		t.Fatalf("unexpected Packet command:\n- want: %v\n-  got: %v", want, got)
+	}
+
+	if want, got := true, pkt.Expect; want != got {
+		t.Fatalf("unexpected Packet.Expect:\n- want: %v\n-  got: %v", want, got)
+	}
+}
+
+func TestEncodeSetRequest(t *testing.T) {
+
+	errTestMarshal := errors.New("error during marshal")
+
+	tests := []struct {
+		name string
+		sr   SetRequest
+		pkt  Packet
+		err  error
+	}{
+		{
+			name: "setSpeedT",
+			sr:   setSpeedT{Speed: 3},
+			pkt:  Packet{Command: 0x99, Data: []byte{3}, Expect: false},
+		},
+		{
+			name: "mockSetReq unknown type",
+			sr:   mockSetReq{mockType: 0},
+			err:  errRequestType,
+		},
+		{
+			name: "mockSetReq marshal error",
+			sr: mockSetReq{
+				mockType: 0xff,
+				mockErr:  errTestMarshal,
+			},
+			err: errTestMarshal,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			pkt, err := EncodeSetRequest(tt.sr)
+
+			if want, got := tt.err, err; want != got {
+				t.Fatalf("unexpected error encoding request:\n- want: %v\n-  got: %v",
+					want, got)
+			}
+
+			if want, got := tt.pkt, pkt; !reflect.DeepEqual(want, got) {
+				t.Fatalf("unexpected request encoding result:\n- want: %v\n-  got: %v",
+					want, got)
+			}
+		})
+	}
+}
+
+func TestDecodeResponse(t *testing.T) {
+
+	tests := []struct {
+		name string
+		pkt  Packet
+		resp Response
+		err  error
+	}{
+		{
+			name: "Bypass",
+			pkt:  Packet{Command: 0xE0, Data: []byte{0xFF, 0xFF, 0xF0, 0x90, 0x01, 0xFF, 1}},
+			resp: &Bypass{
+				Factor:     240,
+				Level:      144,
+				Correction: 1,
+				SummerMode: true,
+			},
+		},
+		{
+			name: "response with nil command",
+			pkt:  Packet{},
+			err:  errDecodeNil,
+		},
+		{
+			name: "non-existing response type",
+			pkt:  Packet{Command: 0xFF},
+			err:  errResponseType,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			resp, err := DecodeResponse(tt.pkt)
+
+			if want, got := tt.err, err; want != got {
+				t.Fatalf("unexpected error decoding response:\n- want: %v\n-  got: %v",
+					want, got)
+			}
+
+			if want, got := tt.resp, resp; !reflect.DeepEqual(want, got) {
+				t.Fatalf("unexpected response decoding result:\n- want: %v\n-  got: %v",
+					want, got)
+			}
+		})
+	}
+}
+
+func TestConnQuery(t *testing.T) {
+
+	srtests := []struct {
+		name string
+		sr   SetRequest
+		tc   TestConn
+		err  error
+	}{
+		{
+			name: "set comfort temperature",
+			sr:   setComfortT{Temperature: 42},
+			tc: TestConn{
+				Receives: []byte{
+					0x07, 0xF0, // Frame Start
+					0x00, uint8(setComfort), // request type
+					0x01, 0x7C, // Length + payload (calculated temp)
+					0xFD,     // Checksum
+					esc, end, // Frame End
+					esc, ack, // Ack
+				},
+				Emits: []byte{esc, ack}, // Read an ACK back from the connection
+			},
+		},
+		{
+			name: "set fan speed level",
+			sr:   setSpeedT{Speed: 3},
+			tc: TestConn{
+				Receives: []byte{
+					0x07, 0xF0, // Start
+					0x00, uint8(setSpeed), // request type
+					0x01, 0x03, // Length + payload
+					0x4A, // Checksum
+					esc, end,
+					esc, ack,
+				},
+				Emits: []byte{esc, ack},
+			},
+		},
+		{
+			name: "garbage before response ACK",
+			sr:   setComfortT{Temperature: 42},
+			tc: TestConn{
+				Receives: []byte{
+					0x07, 0xF0, // Start
+					0x00, uint8(setComfort), // request type
+					0x01, 0x7C, // Length + payload
+					0xFD, // Checksum
+					esc, end,
+					esc, ack,
+				},
+				Emits: []byte{0xF0, 0x0B, 0xA1, 0x2, esc, ack},
+			},
+			err: errScanInput,
+		},
+	}
+
+	for _, tt := range srtests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			// Run setQuery against a test connection
+			err := setQuery(tt.sr, &tt.tc)
+
+			if want, got := tt.err, err; want != got {
+				t.Fatalf("unexpected error during request:\n- want: %v\n-  got: %v",
 					want, got)
 			}
 		})
